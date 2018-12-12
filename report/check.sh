18b48433ac123ff5fcc1c90ecae6ab0cb2d90cc4
sksat <sksat.tec@gmail.com>
2018-12-12 16:11:17 +0900

    update digest

--- report/check.sh
+++ report/check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 digest=`cat digest.txt | cpp -P`
-num=`echo -n $digest | tr -d ' ' | wc -m`
-echo 文字数: $num "(150字程度)"
+digest=`echo -n $digest | tr -d ' '`
+num=`echo -n $digest | wc -m`
+hankaku=`echo -n $digest | tr -d "[:alnum:]" | wc -m`
+#echo $num
+#echo $hankaku
+echo 文字数: `expr $num - \( $num - $hankaku \) / 2` "(150字程度)"
 if [ $num -gt 200 ]; then
 	echo error
 	exit -1
